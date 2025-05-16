f3e88eb13473aefaf3b6f91435e9b26c9b660db9
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 03:25:45 +0700

    bottom sheet

---
+++ MapsBottomSheet/MapsBottomSheet/MapsBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsBottomSheetApp.swift
+//  MapsBottomSheet
+//
+//  Created by Adrian Suryo Abiyoga on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MapsBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
